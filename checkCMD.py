7ff404d9d4dd63fd6f0c4a7627f50ef63746adbf
gpiopi <gpiopi@sina.com>
2018-01-18 07:36:09 +0000

    push

--- checkCMD.py
+++ checkCMD.py
@@ -1,5 +1,6 @@
 from os import system
 import json
+import time
 return_text=system("git pull |grep cmd.json")
 if (return_text==256):
     print ("no new cmd")
@@ -9,6 +10,8 @@ else:
     cmd=cmd_json["cmd"]
     if(cmd=="take-a-pic"):
         print("take a pic")
+        system("fswebcam -d /dev/video0 %s/pics/%s.jpg" % (os.getcwd(),int(time.time())))
+        system("%s/push.sh" % (os.getcwd()))
     if(cmd=="turn-on-light"):
         print("turn on light")
     if(cmd=="turn-off-light"):
