1692792b56b4e9c697f6de5cf773145ca7eb494c
gpiopi <gpiopi@sina.com>
2018-01-18 07:12:40 +0000

    push

--- checkCMD.py
+++ checkCMD.py
@@ -1,12 +1,12 @@
-from os import *
+from os import system
 import json
 return_text=system("git pull |grep cmd.json")
 if (return_text==256):
     print ("no new cmd")
 else:
-    cmd_f=open("cmd.json")
+    cmd_f=open("cmd.json","r")
     cmd_json=json.load(cmd_f)
-    cmd=json["cmd"]
+    cmd=cmd_json["cmd"]
     if(cmd=="take-a-pic"):
         print("take a pic")
     if(cmd=="turn-on-light"):
